5bc36599dd2cc6c8749d516d3ffabcf4a59fbd12
Dan Reynolds <me@danreynolds.ca>
2018-06-12 10:52:06 -0700

    return promise for history jsm methods

--- src/plugin/history.js
+++ src/plugin/history.js
@@ -1,83 +1,78 @@
-'use strict'
+"use strict";
 
 //-------------------------------------------------------------------------------------------------
 
-var camelize = require('../util/camelize');
+var camelize = require("../util/camelize");
 
 //-------------------------------------------------------------------------------------------------
 
-module.exports = function(options) { options = options || {};
+module.exports = function(options) {
+  options = options || {};
 
-  var past       = camelize(options.name || options.past   || 'history'),
-      future     = camelize(                options.future || 'future'),
-      clear      = camelize.prepended('clear', past),
-      back       = camelize.prepended(past,   'back'),
-      forward    = camelize.prepended(past,   'forward'),
-      canBack    = camelize.prepended('can',   back),
-      canForward = camelize.prepended('can',   forward),
-      max        = options.max;
+  var past = camelize(options.name || options.past || "history"),
+    future = camelize(options.future || "future"),
+    clear = camelize.prepended("clear", past),
+    back = camelize.prepended(past, "back"),
+    forward = camelize.prepended(past, "forward"),
+    canBack = camelize.prepended("can", back),
+    canForward = camelize.prepended("can", forward),
+    max = options.max;
 
   var plugin = {
-
     configure: function(config) {
       config.addTransitionLifecycleNames(back);
       config.addTransitionLifecycleNames(forward);
     },
 
     init: function(instance) {
-      instance[past]   = [];
+      instance[past] = [];
       instance[future] = [];
     },
 
     lifecycle: function(instance, lifecycle) {
-      if (lifecycle.event === 'onEnterState') {
+      if (lifecycle.event === "onEnterState") {
         instance[past].push(lifecycle.to);
-        if (max && instance[past].length > max)
-          instance[past].shift();
+        if (max && instance[past].length > max) instance[past].shift();
         if (lifecycle.transition !== back && lifecycle.transition !== forward)
           instance[future].length = 0;
       }
     },
 
-    methods:    {},
+    methods: {},
     properties: {}
-
-  }
+  };
 
   plugin.methods[clear] = function() {
-    this[past].length = 0
-    this[future].length = 0
-  }
+    this[past].length = 0;
+    this[future].length = 0;
+  };
 
   plugin.properties[canBack] = {
     get: function() {
-      return this[past].length > 1
+      return this[past].length > 1;
     }
-  }
+  };
 
   plugin.properties[canForward] = {
     get: function() {
-      return this[future].length > 0
+      return this[future].length > 0;
     }
-  }
+  };
 
   plugin.methods[back] = function() {
-    if (!this[canBack])
-      throw Error('no history');
+    if (!this[canBack]) throw Error("no history");
     var from = this[past].pop(),
-        to   = this[past].pop();
+      to = this[past].pop();
     this[future].push(from);
-    this._fsm.transit(back, from, to, []);
-  }
+    return this._fsm.transit(back, from, to, []);
+  };
 
   plugin.methods[forward] = function() {
-    if (!this[canForward])
-      throw Error('no history');
+    if (!this[canForward]) throw Error("no history");
     var from = this.state,
-        to = this[future].pop();
-    this._fsm.transit(forward, from, to, []);
-  }
+      to = this[future].pop();
+    return this._fsm.transit(forward, from, to, []);
+  };
 
   return plugin;
-
-}
+};
